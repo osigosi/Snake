1126480783277ce7f5edecde9cbd29723c2450e5
osigosi <waltercandy27@gmail.com>
2025-10-03 16:20:19 +0000

    Första inställningarna

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define HEIGHT 480
+#define WIDTH 320
+#define GRID_SIZE 20
+#define COLS (HEIGHT / GRID_SIZE)
+#define ROWS (WIDTH / GRID_SIZE)
+#define MAX_PART (ROWS * COLUMNS)
+
+#endif
---
+++ snake.h
@@ -0,0 +1,3 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
