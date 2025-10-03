82f15f4e35946482de1825374b57391ea93d8446
osigosi <waltercandy27@gmail.com>
2025-10-03 16:34:30 +0000

    Klar

--- config.h
+++ config.h
@@ -6,6 +6,6 @@
 #define GRID_SIZE 20
 #define COLS (HEIGHT / GRID_SIZE)
 #define ROWS (WIDTH / GRID_SIZE)
-#define MAX_PART (ROWS * COLUMNS)
+#define MAX_PART (ROWS * COLS)
 
 #endif
--- snake.h
+++ snake.h
@@ -1,3 +1,17 @@
+#include "config.h"
 #ifndef SNAKE_H
 #define SNAKE_H
 
+typedef struct Body{
+    int x;
+    int y;
+    int color;
+} Body;
+
+typedef struct Snake {
+     Body parts[MAX_PART];  
+    int length;
+} Snake;
+
+
+#endif
